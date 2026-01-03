32832243f129b698fc4b46fa67ff2e899cd81e41
zonary123 <carlosvarasalonso10@gmail.com>
2026-01-03 01:45:01 +0100

    Add asynchronous player data handling and lightweight bytecode optimization

--- src/main/java/org/kingpixel/flancross/Flancross.java
+++ src/main/java/org/kingpixel/flancross/Flancross.java
@@ -1,21 +1,26 @@
 package org.kingpixel.flancross;
 
-import io.github.flemmli97.flan.event.PlayerEvents;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.github.flemmli97.flan.player.PlayerClaimData;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
-import org.apache.logging.log4j.core.jmx.Server;
 import org.kingpixel.flancross.config.Config;
-import org.kingpixel.flancross.database.DataBaseClient;
 import org.kingpixel.flancross.database.DataBaseFactory;
 
-import java.io.FileNotFoundException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Flancross implements ModInitializer {
   public static Config config = new Config();
   public static MinecraftServer server;
+  private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder()
+    .setDaemon(true)
+    .setNameFormat("flancross-executor-%d")
+    .build());
 
   @Override public void onInitialize() {
     config.init();
@@ -24,16 +29,29 @@ public class Flancross implements ModInitializer {
       DataBaseFactory.init();
     });
 
-    ServerPlayerEvents.JOIN.register((player) -> {
+    ServerPlayerEvents.JOIN.register((player) -> runAsync(() -> {
       DataBaseFactory.INSTANCE.loadPlayerData(player);
-    });
+    }));
 
-    ServerPlayerEvents.LEAVE.register((player) -> {
+    ServerPlayerEvents.LEAVE.register((player) -> runAsync(() -> {
       DataBaseFactory.INSTANCE.saveOrUpdate(PlayerClaimData.get(player), player);
-    });
+    }));
 
     ServerLifecycleEvents.SERVER_STOPPING.register((server) -> {
       DataBaseFactory.INSTANCE.disconnect();
     });
+  }
+
+  public static void runAsync(Runnable runnable) {
+    if (EXECUTOR_SERVICE.isShutdown() || EXECUTOR_SERVICE.isTerminated()) {
+      runnable.run();
+      return;
+    }
+    CompletableFuture.runAsync(runnable, EXECUTOR_SERVICE)
+      .orTimeout(30, TimeUnit.SECONDS)
+      .exceptionallyAsync(e -> {
+        e.printStackTrace();
+        return null;
+      });
   }
 }
